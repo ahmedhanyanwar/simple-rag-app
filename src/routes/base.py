d024c18098d0ec8989e4a416bf626973ba18c52b
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2025-03-18 17:41:26 +0200

    add class to setting

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from helpers.config import get_settings
+from helpers.config import get_settings, settings
 
 base_router = APIRouter(
     prefix="/api/v1",    # So now http://localhost:5000/api/v1/ 
@@ -7,7 +7,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/") # default route
-async def wolcome(app_settings=Depends(get_settings)):
+async def wolcome(app_settings: settings=Depends(get_settings)):
 
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
