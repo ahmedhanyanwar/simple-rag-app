29e3c9074b6e497dee6e087740d21552e4084735
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2025-03-18 17:06:08 +0200

    simplify main and create routes folder

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")  #load in main so all modules can see
+
+from routes import base
+
 app = FastAPI()
 
-
-@app.get("/welcome")
-def wolcome():
-    return {
-        "massage": "Hello World!"
-    }
-    
-
+app.include_router(base.base_router)
