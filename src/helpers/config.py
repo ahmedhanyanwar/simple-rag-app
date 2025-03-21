0eec67b36641945a238f21036ff4f11c3305616a
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2025-03-22 01:44:27 +0200

    create mongodb

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -10,6 +10,9 @@ class settings(BaseSettings):
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
 
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
     
 def get_settings():
