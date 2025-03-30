99d63ddb9dc8061b952bbc86673ffcfbdf37a435
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2025-03-30 08:03:13 +0200

    add env LLM config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,7 +4,6 @@ class settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION: str
-    OPENAI_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
@@ -12,6 +11,22 @@ class settings(BaseSettings):
 
     MONGODB_URL: str
     MONGODB_DATABASE: str
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
     
