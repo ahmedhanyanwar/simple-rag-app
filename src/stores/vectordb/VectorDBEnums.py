ced1a4a85f8ab88f9ca518d36cf29fd1ba56b337
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2025-04-20 08:17:32 +0200

    Adding pgvector Enums

--- src/stores/vectordb/VectorDBEnums.py
+++ src/stores/vectordb/VectorDBEnums.py
@@ -2,8 +2,25 @@ from enum import Enum
 
 class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "dot"
 
+# Name of columns
+class PgVectorTableSchemaEnums(Enum):
+    ID = "id"
+    TEXT = "text"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector"
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_l2_ops"
+
+class PgVectorIndexTypeEnums(Enum):
+    HNSW = "hnsw" # 50x faster
+    IVFFLAT = "ivfflat" # This is the default "greedy" which is slow
